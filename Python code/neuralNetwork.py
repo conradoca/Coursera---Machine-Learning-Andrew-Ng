83ed44ea9f158384a2978cbc06dc4a0545d620e4
Conrado Camacho <conradoca@yahoo.com>
2022-02-06 12:49:24 +0100

    Completing NN prediction

--- Python code/neuralNetwork.py
+++ Python code/neuralNetwork.py
@@ -29,34 +29,6 @@ def costFunction(theta, X, y, lmbd=0):
     return J, grad
 
 
-# Predict whether the label is 0 or 1 using learned logistic regression parameters theta
-def predict(theta, X):
-    pred = [sigmoid(X.dot(theta)) >= 0.5]
-    return pred
-
-
-# Feature mapping function to polynomial features
-# maps the two input features to quadratic features used in the regularization exercise.
-
-# Returns a new feature array with more features, comprising of X1, X2, X1.^2, X2.^2, X1*X2, X1*X2.^2, etc..
-# Inputs X1, X2 must be the same size
-# Degree determines the highest degree of combination of the two variables
-def mapFeature(X1, X2, degree=1):
-    out = np.ones(X1.shape[0])[:, np.newaxis]
-    for i in range(1, degree+1):
-        for j in range(0, i+1):
-            out = np.hstack((out, (np.multiply(np.power(X1, i-j), np.power(X2, j))[:, np.newaxis])))
-    return out
-
-
-def mapFeatureForPlotting(X1, X2, degree=1):
-    out = np.ones(1)
-    for i in range(1, degree+1):
-        for j in range(0, i+1):
-            out = np.hstack((out, (np.multiply(np.power(X1, i-j), np.power(X2, j)))))
-    return out
-
-
 # Feedforward propagation algorithm
 # Generic function that can work with any shape of NN
 # Theta = an array that contains the Theta[i] arrays for the various layers i
@@ -66,7 +38,7 @@ def mapFeatureForPlotting(X1, X2, degree=1):
 # Pred outputs the array with the probabilities for being each value
 def feedForwardPropagation(Theta, X, nnDef):
     # Useful variables
-    (m, n) = X.shape                   # m = number of training examples, n = number of features
+    (m, n) = X.shape    # m = number of training examples, n = number of features
     num_labels = nnDef[-1]             # Output Layer units
     num_layers = nnDef.size            # Number of layers including the Input Layer
     p = np.zeros((1, num_labels))       # Classification vector
@@ -80,9 +52,121 @@ def feedForwardPropagation(Theta, X, nnDef):
         # Add the bias unit to the activation
         mLayer = layerActiv[i].shape[0]
         layerActiv[i] = np.append(np.ones((mLayer, 1)), layerActiv[i], axis=1)
-        layerActiv[i+1] = sigmoid(np.dot(layerActiv[i], Theta[i].T))
+        layerActiv[i+1] = sigmoid(np.dot(layerActiv[i], Theta[i+1].T))
 
     Prob = layerActiv[-1]
     p = np.argmax(layerActiv[-1], axis=1)
 
     return Prob, p
+
+
+# Randomly initialize the weights of a layer with L_in
+# incoming connections and L_out outgoing connections
+def randInitializeWeights(L_in, L_out):
+    # We are adding 1 row to take in consideration the bias unit
+    W = np.zeros((L_out, 1 + L_in))
+
+    # The random function provides random values uniformly distributed between [0,1)
+    # We'll multiply by a very small value (epsilon) to make sure the output is made
+    # of very small values
+    eps = 0.12
+
+    # We get a testCases random datapoints. First we get a number of testCases indices out of m
+    W = np.random.rand(L_out, 1 + L_in)*(2*eps)-eps
+    return W
+
+
+# Randomly initialize the Theta array of arrays
+# Remember that to maintain coherence with the notation we start
+# with Theta[1], which represents the input  to first hidden layer
+def initializeTheta(nnDef):
+
+    Theta = np.zeros(nnDef.size, dtype=np.ndarray)
+
+    for i in range(1, nnDef.shape[0]):
+        Theta[i] = randInitializeWeights(nnDef[i-1], nnDef[i])
+    return Theta
+
+
+# Function that converts the Theta array of arrays
+# in a 1D vector. This is required to be sent to
+# the minimize sCiPy function
+def ThetaTo1D(Theta, nnDef):
+    # Useful variables
+    num_layers = nnDef.size            # Number of layers including the Input Layer
+
+    Theta1D = Theta[1].reshape(-1)
+    for i in range(2, num_layers):
+        Theta1D = np.concatenate((Theta1D, Theta[i].reshape(-1)), axis=None)
+
+    return Theta1D
+
+
+# Function that converts back Theta from a 1D vector
+# into an array of arrays
+def ThetaFrom1D(Theta1D, nnDef):
+    # Useful variables
+    num_layers = nnDef.size            # Number of layers including the Input Layer
+
+    Theta = np.zeros(nnDef.size, dtype=np.ndarray)
+    start = 0
+    for i in range(1, num_layers):
+        Theta[i] = Theta1D[start:(start + (nnDef[i] * (nnDef[i-1]+1)))].reshape(nnDef[i], nnDef[i-1]+1)
+        start = start + (nnDef[i] * (nnDef[i-1]+1)+1) - 1
+    return Theta
+
+# NNCOSTFUNCTION Implements the neural network cost function for the
+# neural network which performs classification
+# J, grad = nnCostFunction(Theta, nnDef, X, y, lmbd)
+# computes the cost and gradient of the neural network
+# We are passing the Theta values as a single vector
+# because the minimize function only works with 1-D vectors.
+# That means that the function must reconstruct Theta as a first step
+def nnCostFunction(Theta1D, nnDef, X, y, lmbd):
+    # Useful variables
+    (m, n) = X.shape                   # m = number of training examples, n = number of features
+    num_labels = nnDef[-1]             # Output Layer units
+    num_layers = nnDef.size            # Number of layers including the Input Layer
+    Theta = ThetaFrom1D(Theta1D, nnDef)
+
+    # a array containing the activation arrays
+    # (using numbering from 1 to be coerent with notation)
+    a = np.zeros((nnDef.size+1,), dtype=np.ndarray)
+
+    a[1] = X                # The activation for the Input layer is X
+
+    # Extending the y vector into an array where 1 representents the label
+    y10 = np.zeros((m, num_labels))
+    y = y[:, np.newaxis]
+    for i in range(num_labels):
+        y10[:, i][:, np.newaxis] = np.where(y == i, 1, 0)
+
+    # Forward Propagation
+    for i in range(1, num_layers):
+        # Add the bias unit to the a layer
+        mLayer = a[i].shape[0]
+        a[i] = np.append(np.ones((mLayer, 1)), a[i], axis=1)
+        a[i+1] = sigmoid(np.dot(a[i], Theta[i].T))
+
+    # Cost Function
+    J = (-1/m)*np.sum((np.multiply(np.log(a[num_layers]), y10) + np.multiply((1 - y10), np.log(1-a[num_layers]))))
+    # Cost adding regularization
+    for i in range(1, num_layers):
+        J = J + (lmbd/(2*m))*(np.sum((np.power(Theta[i][:, 1:], 2))))
+
+    # Getting the gradient
+    grad = np.zeros((Theta.shape), dtype=np.ndarray)
+    delta = np.zeros((nnDef.size+1,), dtype=np.ndarray)
+
+    delta[num_layers] = (a[num_layers] - y10)
+    for i in reversed(range(2, num_layers)):
+        delta[i] = (np.dot(delta[i+1], Theta[i]))*(a[i]*(1-a[i]))
+        delta[i] = delta[i][:, 1:]
+
+    # Regularization part of the gradient
+    for i in reversed(range(1, num_layers)):
+        grad[i] = ((1/m)*np.dot(delta[i+1].T, a[i])) + ((lmbd/m)*np.hstack((np.zeros((Theta[i].shape[0], 1)), Theta[i][:,1:])))
+
+    grad1D = ThetaTo1D(grad, nnDef)
+
+    return J, grad1D
